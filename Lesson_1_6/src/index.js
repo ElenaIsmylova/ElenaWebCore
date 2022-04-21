7c9c272c43ad5b9ad49364eacab2df0b899ca8fd
Elena Izmaylova <elena17fine@rambler.ru>
2022-04-21 15:14:08 +0300

    Модуль 1-6

---
+++ Lesson_1_6/src/index.js
@@ -0,0 +1,7 @@
+// Стили
+import 'normalize.css';
+import './styles/main.scss';
+
+// Скрипты
+import './js/swiper.js';
+import './js/modules.js';
---
+++ Lesson_1_6/src/js/modules.js
@@ -0,0 +1,38 @@
+// Бургер-меню
+let open_menu = document.querySelector('.hamburger')
+let hamburger = document.querySelector('.hamburger-menu')
+let close_menu = document.querySelector('.hamburger-menu__burger')
+
+open_menu.addEventListener('click', function () {
+    hamburger.classList.add('visible')
+})
+
+close_menu.addEventListener('click', function () {
+    hamburger.classList.remove('visible')
+})
+
+// Окно обратной связи
+let open_feedback = document.querySelector('.header__chat')
+let feedback = document.querySelector('.feedback')
+let close_feedback = document.querySelector('.feedback__close')
+
+open_feedback.addEventListener('click', function () {
+    feedback.classList.add('visible')
+})
+
+close_feedback.addEventListener('click', function () {
+    feedback.classList.remove('visible')
+})
+
+// Окно "Заказать звонок"
+let open_callback = document.querySelector('.header__phone')
+let callback = document.querySelector('.callback')
+let close_callback = document.querySelector('.callback__close')
+
+open_callback.addEventListener('click', function () {
+    callback.classList.add('visible')
+})
+
+close_callback.addEventListener('click', function () {
+    callback.classList.remove('visible')
+})

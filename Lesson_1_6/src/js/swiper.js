62fb2e25fc30487d0945bc4e9431ab43b559758d
Elena Izmaylova <elena17fine@rambler.ru>
2022-04-22 17:25:20 +0300

    Module 1-6

--- Lesson_1_6/src/js/swiper.js
+++
@@ -1,9 +0,0 @@
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 1.2,
-    loop: true,
-    loopedSlides: 1,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    }   
-});
